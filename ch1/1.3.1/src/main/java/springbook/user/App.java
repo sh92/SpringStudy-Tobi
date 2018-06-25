1af90b950cbaacdaf9e9531094867453652bb7bf
sh92 <sanghee.lee1992@gmail.com>
2018-06-25 10:36:48 +0900

    Extension of DAO

---
+++ ch1/1.3.1/src/main/java/springbook/user/App.java
@@ -0,0 +1,21 @@
+package springbook.user;
+
+import java.sql.SQLException;
+
+import springbook.user.dao.NUserDao;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+public class App {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao dao = new NUserDao();
+		User user = new User();
+
+		user.setId("myid");
+		user.setName("myName");
+		user.setPassword("myPassword");
+		dao.add(user);
+
+		System.out.println("My id is " + user.getId());
+	}
+}
---
+++ ch1/1.3.1/src/main/java/sql/create_databse.sql
@@ -0,0 +1,3 @@
+create databse springbook;
+create user 'spring'@'localhost' identified by 'book';
+GRANT ALL PRIVILEGES ON * . * TO 'spring'@'localhost';
