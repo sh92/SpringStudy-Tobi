f8dc6c17d837819186f4663944d1662a7d2a25d9
sh92 <sanghee.lee1992@gmail.com>
2018-06-26 22:42:41 +0900

    Add getAll

---
+++ ch3/3.6/src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,94 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import springbook.user.domain.User;
+
+public class UserDao {
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public void add(final User user) throws SQLException {
+		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(),
+				user.getPassword());
+	}
+
+	public User get(String id) throws SQLException {
+		Connection c = this.dataSource.getConnection();
+		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+		ps.setString(1, id);
+
+		ResultSet rs = ps.executeQuery();
+
+		User user = null;
+		if (rs.next()) {
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+		}
+
+		rs.close();
+		ps.close();
+		c.close();
+
+		if (user == null)
+			throw new EmptyResultDataAccessException(1);
+
+		return user;
+	}
+
+	public void deleteAll() throws SQLException {
+		this.jdbcTemplate.update("delete from users");
+	}
+
+	public int getCount() throws SQLException {
+		Connection c = dataSource.getConnection();
+
+		PreparedStatement ps = c.prepareStatement("select count(*) from users");
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		int count = rs.getInt(1);
+
+		rs.close();
+		ps.close();
+		c.close();
+
+		return count;
+	}
+
+	public List<User> getAll() {
+		return this.jdbcTemplate.query("select * from users order by id", new RowMapper<User>() {
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+				return user;
+			}
+		});
+	}
+
+	public void deleteAll1() {
+		this.jdbcTemplate.update("delete from users");
+	}
+}
