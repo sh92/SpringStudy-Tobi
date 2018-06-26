9278fd4484db66645166506432ad04054f3fc1b2
sh92 <sanghee.lee1992@gmail.com>
2018-06-26 17:08:43 +0900

    Calculator Unit Test by using tempmlate/callback

---
+++ ch3/3.5/src/main/java/springbook/learningtest/template/CalcSumTest.java
@@ -0,0 +1,35 @@
+package springbook.learningtest.template;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalcSumTest {
+	Calculator calculator;
+	String numFilepath;
+
+	@Before
+	public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilepath = getClass().getResource("number.txt").getPath();
+	}
+
+	@Test
+	public void sumeNumbers() throws IOException {
+		assertThat(calculator.calcSsum(this.numFilepath), is(10));
+	}
+
+	@Test
+	public void multiplyOfNumbers() throws IOException {
+		assertThat(calculator.calcMultiply(this.numFilepath), is(24));
+	}
+
+	@Test
+	public void concatenateStrings() throws IOException {
+		assertThat(calculator.concatenate(this.numFilepath), is("1234"));
+	}
+}
