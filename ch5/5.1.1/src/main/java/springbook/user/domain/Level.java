72e9f1dd4f73e0bb6a084ccd4cf8d104f50633df
sh92 <sanghee.lee1992@gmail.com>
2018-06-27 13:05:44 +0900

    Add field(Abstraction of Service)

---
+++ ch5/5.1.1/src/main/java/springbook/user/domain/Level.java
@@ -0,0 +1,29 @@
+package springbook.user.domain;
+
+public enum Level {
+	BASIC(1), SILVER(2), GOLD(3);
+
+	private final int value;
+
+	private Level(int value) {
+		this.value = value;
+	}
+
+	public int intValue() {
+		return value;
+	}
+
+	public static Level valueOf(int value) {
+		switch (value) {
+		case 1:
+			return Level.BASIC;
+		case 2:
+			return Level.SILVER;
+		case 3:
+			return Level.GOLD;
+		default:
+			throw new AssertionError("Unknown value: " + value);
+		}
+	}
+
+}
